cfeeeb855b18d0a189d2f72a96474ed33ee2975e
taow <taow@utexas.edu>
2014-09-16 00:35:40 -0500

    separate side panel

--- 04Shiny/ui.R
+++ 04Shiny/ui.R
@@ -3,28 +3,44 @@ library(shiny)
 shinyUI(fluidPage(
 	titlePanel("Population Data of the US"),
 
-	sidebarLayout(
+	sidebarLayout(position = "left",
     
 		sidebarPanel(
    		  helpText("Display the population allocation of US in 2010 by state"),
 	      selectInput("var", 
        		 label = "Choose the type of populaiton to display",
        		 choices = list("Total", "White", "Black or African American", "American Indian and Alaska Native", "Asian", "Hispanic", "Native Hawaiian and Other Pacific Islander"),
-	        selected = "Total"),
-         helpText("Display age distribution of different state"),
-   		  selectInput("var2", 
-   		              label = "Choose the state to display",
-   		              choices = list("TX", "CA"),
-   		              selected = "TX")
+	        selected = "Total")
+
+ 
          
       ),
     
 		mainPanel(
-		  plotOutput("disPlot"),
-      plotOutput("disPlot2")
+		  plotOutput("disPlot")
 		)    
 
+	),
+	sidebarLayout(position = "left",
+	              
+	              sidebarPanel(
+	                
+	                
+	                helpText("Display age distribution of different state"),
+	                selectInput("var2", 
+	                            label = "Choose the state to display",
+	                            choices = list("TX", "CA"),
+	                            selected = "TX")
+	                
+	              ),
+	              
+	              mainPanel(
+	                plotOutput("disPlot2")
+	              )    
+	              
 	)
+
+  
 
 
 ))
